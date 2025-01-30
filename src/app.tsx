2c7d1d025947261e6516909fbfe2370d067143b2
TamaraRibeiro <tamara.fl@hotmail.com>
2025-01-30 16:17:54 -0300

    :tada: first commit - setting up project and configs

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  
+  return (
+  <div className="bg-red-50/40 h-screen w-screen flex items-center justify-center">
+    <h1 className="text-4xl font-semibold leading-loose text-violet-400">Frontend Segmentation Labeling Tool for Overview</h1>
+  </div>
+);
+}
